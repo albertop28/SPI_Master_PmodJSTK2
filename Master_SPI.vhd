1bf6e92bd4497e2092dc3cb700a1dc2b5604231e
Néstor Ruiz <nestoralbertoruiz@gmail.com>
2024-10-14 23:18:50 -0600

    dos joysticks al 75% de funcionamiento

--- Master_SPI.vhd
+++ Master_SPI.vhd
@@ -1,29 +1,31 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.std_logic_arith.all;
-USE ieee.std_logic_unsigned.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 
 entity Master_SPI is port(
-	mosi, cs1, sclk, psh1, psh2 : out std_logic; --señales de salida mosi, ChipSelect y señal de reloj
-	miso, clk, reset_n : in std_logic; --señales de entrada miso, reloj de fpga y reset
-	LED_Data_1, LED_Data_2 : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
+	push_btns : out std_logic_vector(1 downto 0);
+	mosi, cs1, sclk : out std_logic; 
+	miso, clk, reset_n : in std_logic; 				--señales de entrada miso, reloj de fpga y reset
+	LED_Data_1, LED_Data_2 : OUT STD_LOGIC_VECTOR(3 DOWNTO 0));
 end Master_SPI;
 
 architecture Behavioral of Master_SPI is
-	signal clk_signal, reloj: std_logic := '0';
+	signal clk_signal, reloj: std_logic := '1';
 	signal cont : integer := 1;
-	signal byte_received : std_logic_vector(39 downto 0):= "0000000000000000000000000000000000000001";
-	signal cont_byte : integer range 0 to 40 := 0;
-	constant command_pos : std_logic_vector(7 downto 0):= "10000001";
-	constant command_led_green : std_logic_vector(7 downto 0):= "00000011";
-begin
+	signal cont_byte : integer := 0;
+	signal byte_received : std_logic_vector(39 downto 0):= "0000000000000000000000000000000000000000";
+	constant command_pos : std_logic_vector(7 downto 0):= "00000000";
+	constant command_led_green : std_logic_vector(7 downto 0):= "10000001";
+	constant div_freq_val : integer := 10000;
+	begin
 
 clk_signal <= clk;
 
-----------------Divisor de frecuencia maquina de estados----------------
+---------------- Divisor de frecuencia maquina de estados ----------------
 process(clk_signal) begin									--se crea un proceso que ocupa la variable clk (cristal oscilador)
 	if clk_signal'event and clk_signal = '1' then	--cada que la variable clk cambie (de o a 1 o veceversa)
-		if cont = 10000 then			 						--50MHz (de la Amiba2)/ 2 * frecuencia deseada (2500 Hz) = 10000
+		if cont = div_freq_val then			 			--50MHz (de la Amiba2)/ 2 * frecuencia deseada (2500 Hz) = 10000
 			reloj <= not reloj;								--la señal reloj se niega (como se inicializo en 0 ahora cambia a 1)
 			cont <= 1;											--contador lo igualamos otra vez a 1 
 		else									
@@ -32,40 +34,54 @@ process(clk_signal) begin									--se crea un proceso que ocupa la variable clk
 	end if;
 end process;
 
+--------------------------------Main process--------------------------------
 process(reloj, reset_n) begin
 	if reset_n = '1' then
 		cs1 <= '1';
 		sclk <= '0';
 		mosi <= '0';
-	else --reset_n = '0' then
+		--cont_byte <= 0;
+		--byte_received <= "0000000000000000000000000000000000000000";
+	else
 		sclk <= reloj;
-		if reloj'event and reloj = '1' then
-		case cont_byte is
-			when 40 =>
-				cs1 <= '1';
-				mosi <= '0';
-				LED_Data_1 <= byte_received(9 downto 8) & byte_received(23 downto 18);
-            LED_Data_2 <= byte_received(25 downto 24) & byte_received(39 downto 34);
-            psh1 <= byte_received(0);
-            psh2 <= byte_received(1);
-            cont_byte <= 0;
-            --pos_x <= byte_received(35 downto 28);
-            --pos_y <= byte_received(23 downto 16);
-			when 0 to 7 =>
-				cs1 <= '0';
-				mosi <= command_pos(cont_byte);
-            -- Shift bits
-            byte_received <= byte_received(38 downto 0) & miso;
-				cont_byte <= cont_byte + 1;
-			when others =>
-            cs1 <= '0';
-            mosi <= '0';
-            -- Shift bits
-            byte_received <= byte_received(38 downto 0) & miso;
-            cont_byte <= cont_byte + 1;
-			end case;
+		if reloj'event and reloj = '0' then
+			case cont_byte is
+				when 0 | 10 =>
+					cs1 <= '1';
+					mosi <= '0';
+					byte_received <= "0000000000000000000000000000000000000000";
+					cont_byte <= cont_byte + 1;
+				when 1 to 8 =>
+					cs1 <= '0';
+					mosi <= command_led_green(cont_byte-1);
+					byte_received <= not "0000000000000000000000000000000000000000";
+					cont_byte <= cont_byte + 1;
+				when 9 =>
+					cs1 <= '0';
+					mosi <= '0';
+					byte_received <= not "0000000000000000000000000000000000000000";
+					cont_byte <= cont_byte + 1;
+				when 11 to 18 =>
+					cs1 <= '0';
+					mosi <= command_pos(cont_byte-11);
+					-- Shift bits
+					byte_received <= byte_received(38 downto 0) & miso;
+					cont_byte <= cont_byte + 1;
+				when 51 =>
+					cs1 <= '1';
+					mosi <= '0';
+					LED_Data_1 <= byte_received(9 downto 8) & byte_received(23 downto 22);
+					LED_Data_2 <= byte_received(25 downto 24) & byte_received(39 downto 38);
+					push_btns <= byte_received(1 downto 0);
+					cont_byte <= 10;
+				when others =>
+					cs1 <= '0';
+					mosi <= '0';
+					-- Shift bits
+					byte_received <= byte_received(38 downto 0) & miso;
+					cont_byte <= cont_byte + 1;
+				end case;
+			end if;
 		end if;
-	end if;
 end process;
-
 end Behavioral;
---
+++ top.vhd
@@ -0,0 +1,59 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity Top_SPI is port(
+	clk : in std_logic;
+	miso_1 : in std_logic;
+	miso_2 : in std_logic;
+	cs_1, mosi_1, sclk_1, cs_2, mosi_2, sclk_2: out std_logic;
+   joystick_der, joystick_izq : out std_logic_vector(9 downto 0));
+end Top_SPI;
+
+architecture Behavioral of Top_SPI is
+    -- Señales para interconectar las instancias del Master_SPI
+    signal LED_Data_1_1, LED_Data_2_1 : std_logic_vector(3 downto 0) := "0000";
+    signal LED_Data_1_2, LED_Data_2_2 : std_logic_vector(3 downto 0) := "0000";
+	 signal push_der, push_izq : std_logic_vector(1 downto 0) := "00";
+	 signal clk_s1, clk_s2: std_logic := '0';
+begin
+
+clk_s1 <= clk;
+clk_s2 <= clk;
+
+-- Instancia joystick_1 de Master_SPI
+SPI_1: entity work.Master_SPI port map(
+	-- inputs:
+	clk => clk_s1,
+	reset_n => '0',
+	miso => miso_1,
+	-- outputs:
+	mosi => mosi_1,
+	cs1 => cs_1,
+	sclk => sclk_1,
+	LED_Data_1 => LED_Data_1_1,
+	LED_Data_2 => LED_Data_2_1,
+	push_btns => push_der
+);
+
+-- Instancia joystick_2 de Master_SPI
+SPI_2: entity work.Master_SPI port map(
+	-- inputs:
+	clk => clk_s2,
+	reset_n => '0',
+	miso => miso_2,
+	-- outputs:
+	mosi => mosi_2,
+	cs1 => cs_2,
+	sclk => sclk_2,
+	LED_Data_1 => LED_Data_1_2,
+	LED_Data_2 => LED_Data_2_2,
+	push_btns => push_izq
+);
+
+-- Extracción de los 4 bits más importantes de LED_Data_1 y LED_Data_2 para cada joystick
+joystick_der <= push_der & LED_Data_1_1 & LED_Data_2_1;
+joystick_izq <= push_izq & LED_Data_1_2 & LED_Data_2_2;
+
+end Behavioral;
